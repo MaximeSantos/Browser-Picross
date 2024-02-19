94fd240cd6ac64bdb69eaf7b6ab55d4d8f81994c
MaximeSantos <118924091+MaximeSantos@users.noreply.github.com>
2024-02-19 14:24:11 +0100

    Add a simple grid with a table element based on height/width constants

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Table from './Table.svelte';
+
+  // Setting up our variables based on the .non format for now
+  const solution = [0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0];
+  const board = [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3];
+  const height = 5;
+  const width = 5;
+
+  let test = "Hello";
+</script>
+
+<h1>Browser Picross</h1>
+<p>{test}</p>
+<div class="board">
+  <Table {height} {width}/>
+</div>
