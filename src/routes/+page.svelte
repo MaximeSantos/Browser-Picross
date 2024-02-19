7cd52d087087a215d07dfdebe06581075435cf3a
MaximeSantos <118924091+MaximeSantos@users.noreply.github.com>
2024-02-19 18:12:53 +0100

    Add basic grid board

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,26 @@
 <script>
   import Table from './Table.svelte';
+  import Board from './Board.svelte';
 
-  // Setting up our variables based on the .non format for now
+  // Setting up our variables based on the .non format for now (https://webpbn.com/export.cgi amongst many other possibilities)
   const solution = [0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0];
-  const board = [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3];
   const height = 5;
-  const width = 5;
-
-  let test = "Hello";
+  const width = 10;
+  let board = Array.from({length: height*width}, () => 3);
+  console.log(board)
 </script>
 
 <h1>Browser Picross</h1>
-<p>{test}</p>
 <div class="board">
   <Table {height} {width}/>
 </div>
+
+<div class="board">
+  <Board {height} {width} {board}/>
+</div>
+
+<style>
+  .board {
+    margin: 2rem
+  }
+</style>
