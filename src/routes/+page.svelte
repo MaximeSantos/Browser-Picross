6eec53d4c8dfa9764779b130df4f502f3320eda5
MaximeSantos <118924091+MaximeSantos@users.noreply.github.com>
2024-02-19 18:17:17 +0100

    Format with prettier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,26 @@
 <script>
-  import Table from './Table.svelte';
-  import Board from './Board.svelte';
+	import Table from './Table.svelte';
+	import Board from './Board.svelte';
 
-  // Setting up our variables based on the .non format for now (https://webpbn.com/export.cgi amongst many other possibilities)
-  const solution = [0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0];
-  const height = 5;
-  const width = 10;
-  let board = Array.from({length: height*width}, () => 3);
-  console.log(board)
+	// Setting up our variables based on the .non format for now (https://webpbn.com/export.cgi amongst many other possibilities)
+	const solution = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0];
+	const height = 5;
+	const width = 10;
+	let board = Array.from({ length: height * width }, () => 3);
+	console.log(board);
 </script>
 
 <h1>Browser Picross</h1>
 <div class="board">
-  <Table {height} {width}/>
+	<Table {height} {width} />
 </div>
 
 <div class="board">
-  <Board {height} {width} {board}/>
+	<Board {height} {width} {board} />
 </div>
 
 <style>
-  .board {
-    margin: 2rem
-  }
+	.board {
+		margin: 2rem;
+	}
 </style>
--- src/routes/Board.svelte
+++ src/routes/Board.svelte
@@ -1,72 +1,73 @@
-<script lang=ts>
-  export let height: number;
-  export let width: number;
-  export let board: number[];
+<script lang="ts">
+	export let height: number;
+	export let width: number;
+	export let board: number[];
 </script>
 
 <div class="container">
-  <!-- Header Left -->
-  <div class="left-h">
-    {#each {length: height} as _,i}
-      <div>
-        {i}
-      </div>
-    {/each}
-  </div>
+	<!-- Header Left -->
+	<div class="left-h">
+		{#each { length: height } as _, i}
+			<div>
+				{i}
+			</div>
+		{/each}
+	</div>
 
-  <!-- Header Top -->
-  <div class="top-h">
-    {#each {length: width} as _,i}
-      <div>
-        {i}
-      </div>
-    {/each}
-  </div>
+	<!-- Header Top -->
+	<div class="top-h">
+		{#each { length: width } as _, i}
+			<div>
+				{i}
+			</div>
+		{/each}
+	</div>
 
-  <!-- Body of the grid -->
-  <div class="grid" style:--height={height} style:--width={width}>
-    {#each board as cell,i}
-        <div class="cell">{cell}</div>
-    {/each}
-  </div>
+	<!-- Body of the grid -->
+	<div class="grid" style:--height={height} style:--width={width}>
+		{#each board as cell, i}
+			<div class="cell">{cell}</div>
+		{/each}
+	</div>
 </div>
 
 <style>
-  .container {
-    display: grid;
-    grid-template-columns: auto 1fr;
-    grid-template-rows: auto 1fr;
-    grid-template-areas:
-    ". top"
-    "left grid";
-  }
+	.container {
+		display: grid;
+		grid-template-columns: auto 1fr;
+		grid-template-rows: auto 1fr;
+		grid-template-areas:
+			'. top'
+			'left grid';
+	}
 
-  .top-h {
-    grid-area: top;
-    display: flex;
-    flex-direction: row;
-    gap: 1px;
-  }
-  .left-h {
-    display: flex;
-    flex-direction: column;
-    grid-area: left;
-    gap: 1px;
-  }
+	.top-h {
+		grid-area: top;
+		display: flex;
+		flex-direction: row;
+		gap: 1px;
+	}
+	.left-h {
+		display: flex;
+		flex-direction: column;
+		grid-area: left;
+		gap: 1px;
+	}
 
-  .top-h div, .left-h div {
-    height: 2rem;
-    width: 2rem;
-  }
+	.top-h div,
+	.left-h div {
+		height: 2rem;
+		width: 2rem;
+	}
 
-  .cell {
-    /* border: 1px solid #FBFBFE; */
-  }
+	.cell {
+		/* border: 1px solid #FBFBFE; */
+	}
 
-  .grid {
-    grid-area: grid;
-    display: grid;
-    grid-template-columns: repeat(var(--width), 2rem);
-    grid-template-rows: repeat(var(--height), 2rem);
-  }
+	.grid {
+		grid-area: grid;
+		display: grid;
+		grid-template-columns: repeat(var(--width), 2rem);
+		grid-template-rows: repeat(var(--height), 2rem);
+	}
 </style>
