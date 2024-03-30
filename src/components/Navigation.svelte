8f2925e7bc78635c0b871fc18864a4e460663637
MaximeSantos <118924091+MaximeSantos@users.noreply.github.com>
2024-03-30 20:45:55 +0100

    WIP Rework navbar

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -4,35 +4,47 @@
 	export let username: string = '';
 </script>
 
-<nav>
-	{#if isLoggedIn}
-		{#if username}
-			<p>Hello {username} :</p>
+<div class="nav-container">
+	<nav>
+		<a class="route" href="/">Home</a>
+		{#if isLoggedIn && isStaff}
+			<a class="route" href="/admin">Admin</a>
 		{/if}
-		{#if isStaff}
-			<a href="/admin">Admin</a>
+	</nav>
+	<div class="container">
+		{#if isLoggedIn}
+			{#if username}
+				<p>Hello {username} :</p>
+			{/if}
+			<a class="login-logout" href="/logout" data-sveltekit-reload>Logout</a>
+		{:else}
+			<a class="login-logout" href="/login">Login</a>
 		{/if}
-		<a href="/logout" data-sveltekit-reload>Logout</a>
-	{:else}
-		<a href="/login">Login</a>
-	{/if}
-</nav>
+	</div>
+</div>
 
 <style>
+	.nav-container {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		width: 100%;
+	}
+	.container {
+		flex-direction: row;
+	}
 	nav {
-		margin-right: 2dvw;
-		margin-left: auto;
 		display: flex;
 		align-items: center;
 	}
-	a {
+	.login-logout {
 		margin-left: 2rem;
 		padding: 0.4rem 0.5rem;
 		border: 1px solid var(--border-light);
 		border-radius: 0.2rem;
 		background-color: var(--light-dark);
 	}
-	a:hover {
+	.login-logout:hover {
 		background-color: var(--border-light);
 		text-decoration: none;
 	}
