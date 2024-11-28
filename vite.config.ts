f00c63eb9bc4d22691437b0e986eaae0c0b54e91
MaximeSantos <118924091+MaximeSantos@users.noreply.github.com>
2024-11-28 17:11:29 +0100

    Add CORS to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		cors: {
+			origin: process.env.VITE_ALLOWED_ORIGIN,
+			methods: ['GET'],
+			allowedHeaders: ['Content-Type'],
+			credentials: true
+		}
+	}
 });
